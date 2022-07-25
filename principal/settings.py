f7e9122fffbce0f0b374f10418569ddfa914a440
Anderson de Aguiar de Oliveira <90530503+andersonaoliveira@users.noreply.github.com>
2022-07-24 22:55:54 -0300

    Update settings.py

--- principal/settings.py
+++ principal/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         'NAME': 'ifrs',
         'USER': 'postgres',
         'PASSWORD':'123456',
-        'HOST':'localhost',
+        'HOST':'https://db-django-andersonaoliveira.cloud.okteto.net/',
 	'PORT':'5432'
 	
     }
