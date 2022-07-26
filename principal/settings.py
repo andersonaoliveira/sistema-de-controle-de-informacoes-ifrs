a02faae315757c101e03396f1fde8bb2d0e1c46d
Anderson de Aguiar de Oliveira <90530503+andersonaoliveira@users.noreply.github.com>
2022-07-26 10:56:15 -0300

    Update settings.py

--- principal/settings.py
+++ principal/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD':'postgres',
+        'PASSWORD':123456,
         'HOST':'localhost',
 	'PORT':5432
     }
