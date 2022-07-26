4fac92c038eac23ea7df5c6ecff98aa0e9210eaf
Anderson de Aguiar de Oliveira <90530503+andersonaoliveira@users.noreply.github.com>
2022-07-26 11:13:02 -0300

    Update settings.py

--- principal/settings.py
+++ principal/settings.py
@@ -71,11 +71,11 @@ WSGI_APPLICATION = 'principal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_ENV_DB', 'postgres'),
-        'USER': os.environ.get('DB_ENV_POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_ENV_POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_PORT_5432_TCP_ADDR', 'db'),
-        'PORT': os.environ.get('DB_PORT_5432_TCP_PORT', ''),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 'DB_PORT_5432_TCP_PORT',
     },
 }
 
