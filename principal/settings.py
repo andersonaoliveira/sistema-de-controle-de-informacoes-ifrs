d608b954b45cfcbe4ede70d03f9ff8e54916b029
Anderson de Aguiar de Oliveira <90530503+andersonaoliveira@users.noreply.github.com>
2022-07-26 10:53:22 -0300

    Update settings.py

--- principal/settings.py
+++ principal/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'principal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD':'postgres',
