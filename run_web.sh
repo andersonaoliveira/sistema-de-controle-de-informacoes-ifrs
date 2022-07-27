53840030371d836e6a852f9420f7e2b0c131a809
Anderson de Aguiar de Oliveira <90530503+andersonaoliveira@users.noreply.github.com>
2022-07-27 08:45:29 -0300

    Create run_web.sh

---
+++ run_web.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# wait for PSQL server to start
+while ! curl --max-time 30 http://db:5432/ 2>&1 | grep '52'
+do
+    echo "Waiting for database..."
+    sleep 1
+done
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py runserver 0.0.0.0:8080
